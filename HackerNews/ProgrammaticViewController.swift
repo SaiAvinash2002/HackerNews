4fb3e49a033695a5a014e8767f3f2197f1bc7ed2
Sai Avinash <saiavinashvenkata@gmail.com>
2025-08-21 07:20:26 -0500

    added programmatic code for login page

---
+++ HackerNews/ProgrammaticViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProgrammaticViewController.swift
+//  HackerNews
+//
+//  Created by Sai Avinash Vagicherla on 8/20/25.
+//
+
+import UIKit
+
+class ProgrammaticViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
